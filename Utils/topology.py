84068c7e5a3293c43d6df58a844ca32a74dc8ad6
F0x <Operator-F0x@proton.me>
2025-11-07 11:23:42 +0100

    Refactor network topology creation and messages

--- Utils/topology.py
+++ Utils/topology.py
@@ -11,10 +11,10 @@ from Utils.router_ip import get_router_ip
 
 def create_network_topology(local_ip, subnet_mask):
     try:
-        # Trova l'indirizzo IP del router
+       
         router_ip = get_router_ip(local_ip, subnet_mask)
         if router_ip is None:
-            print("Impossibile determinare l'indirizzo IP del router.")
+            print("cant found router ip.")
             return
 
         print(f"Router IP trovato: {router_ip}")
@@ -22,7 +22,7 @@ def create_network_topology(local_ip, subnet_mask):
         active_hosts = arp_scan(local_ip, subnet_mask)
 
         if not active_hosts:
-            print("Nessun host attivo trovato nella rete.")
+            print("No host found.")
             return
         else:
             print(f"{len(active_hosts)} host attivi trovati.")
@@ -45,16 +45,17 @@ def create_network_topology(local_ip, subnet_mask):
 
         nx.draw(G, pos, with_labels=True, node_size=3000, node_color=node_colors, font_size=10, font_weight='bold')
 
-        plt.title(f"Topologia di rete per {local_ip}/{subnet_mask}")
+        plt.title(f"Topology {local_ip}/{subnet_mask}")
         plt.show()
     except Exception as e:
-        print(f"Errore nella creazione della topologia: {e}")
+        print(f"Error: {e}")
 
     
 if __name__ == "__main__":
 
     locip, submask = get_local_ip_and_subnet()
 
-    print(f"IP locale ottenuto: {locip}, Subnet Mask: {submask}")
+    print(f"IP local: {locip}, Subnet Mask: {submask}")
 
     create_network_topology(locip, submask)
+
